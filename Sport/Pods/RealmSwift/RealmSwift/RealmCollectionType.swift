910d22d85843b61e05b39ddb8e0861695db7db77
Hai Le <levuhai@ymail.com>
2016-11-25 14:09:08 +0700

    update pod

--- Sport/Pods/RealmSwift/RealmSwift/RealmCollectionType.swift
+++ Sport/Pods/RealmSwift/RealmSwift/RealmCollectionType.swift
@@ -396,7 +396,7 @@ private final class _AnyRealmCollection<C: RealmCollectionType>: _AnyRealmCollec
         self.base = base
     }
 
-    //TODO: copyedit private docstrings
+    // TODO (az): copyedit private docstrings
     // MARK: Properties
 
     /// The Realm the objects in this collection belong to, or `nil` if the
